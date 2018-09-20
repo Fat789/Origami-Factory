43b871c4a57e143d8b9c59ec771bedbdf0400c6d
Unknown <valentin.peclard@hotmail.fr>
2018-09-20 16:08:27 +0200

    front maj

--- Frontend/Origami-app/src/app/app.module.ts
+++ Frontend/Origami-app/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OrigamiComponent } from './origami/origami.component';
+import { EtapeComponent } from './etape/etape.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OrigamiComponent,
+    EtapeComponent,
+    CategorieComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/Origami-app/src/app/origami/origami.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-origami',
+  templateUrl: './origami.component.html',
+  styleUrls: ['./origami.component.css']
+})
+export class OrigamiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
