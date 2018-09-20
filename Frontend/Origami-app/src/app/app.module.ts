087711053dad8df8f9aa0012d3b6fd64cbe0e3ee
Unknown <valentin.peclard@hotmail.fr>
2018-09-20 16:52:07 +0200

    maj

--- Frontend/Origami-app/src/app/app.module.ts
+++ Frontend/Origami-app/src/app/app.module.ts
@@ -1,32 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { OrigamiComponent } from './origami/origami.component';
-import { EtapeComponent } from './etape/etape.component';
-import { CategorieComponent } from './categorie/categorie.component';
-import { AdminComponent } from './admin/admin.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {OrigamiComponent} from './origami/origami.component';
+import {EtapeComponent} from './etape/etape.component';
+import {CategorieComponent} from './categorie/categorie.component';
+import {AdminComponent} from './admin/admin.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AccueilComponent} from './accueil/accueil.component';
+
+const routes: Routes = [
+    {path: 'accueil', component: AccueilComponent},
+    {path: 'categorie', component: CategorieComponent},
+    {path: 'origami', component: OrigamiComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', redirectTo: 'accueil', pathMatch: 'full'}
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    OrigamiComponent,
-    EtapeComponent,
-    CategorieComponent,
-    AdminComponent,
-    AdminComponent,
-    CategorieComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        OrigamiComponent,
+        EtapeComponent,
+        CategorieComponent,
+        AdminComponent,
+        CategorieComponent,
+        AccueilComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
