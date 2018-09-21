4becb30213c8fa06c8f1fda8fdafc3f422920a67
Adrien <adrien.lavelle@hotmail.fr>
2018-09-21 10:33:13 +0200

    update test 2.0

--- Backend/src/test/java/sopra/promo404/origami/OrigamiApplicationTests.java
+++ Backend/src/test/java/sopra/promo404/origami/OrigamiApplicationTests.java
@@ -38,91 +38,58 @@ public class OrigamiApplicationTests {
 	public void contextLoads() throws ParseException {
 
 		// Etape creation
-		
+
 		Etape e1 = new Etape();
 		e1.setNumeroEtape(1);
 		e1.setDescription(null);
 		e1.setImageEtape(null);
-		
+
 		etapeRepo.save(e1);
-		
+
 		Etape e2 = new Etape();
 		e2.setNumeroEtape(2);
 		e2.setDescription(null);
 		e2.setImageEtape(null);
-			
+
 		etapeRepo.save(e2);
-		
+
 		Etape e3 = new Etape();
 		e3.setNumeroEtape(3);
 		e3.setDescription(null);
 		e3.setImageEtape(null);
-			
+
 		etapeRepo.save(e3);
-		
+
 		Etape e4 = new Etape();
 		e4.setNumeroEtape(4);
 		e4.setDescription(null);
 		e4.setImageEtape(null);
-			
+
 		etapeRepo.save(e4);
-		
-		//Listes d'étapes creation
-		
+
+		// Listes d'étapes creation
+
 		List<Etape> etapes1 = new ArrayList<>();
 		etapes1.add(e1);
 		etapes1.add(e2);
-		
+
 		etapeRepo.saveAll(etapes1);
-		
+
 		List<Etape> etapes2 = new ArrayList<>();
 		etapes2.add(e1);
 		etapes2.add(e2);
 		etapes2.add(e3);
 		etapes2.add(e4);
-		
+
 		etapeRepo.saveAll(etapes2);
-		
+
 		List<Etape> etapes3 = new ArrayList<>();
 		etapes3.add(e3);
 		etapes3.add(e4);
-		
+
 		etapeRepo.saveAll(etapes3);
-		
-		//Image Origami creation
-		
-				
-		// Origami creation
 
-		Origami ori1 = new Origami();
-
-		ori1.setNom("avion1");
-		ori1.setTempsRea(null);
-		ori1.setNbFeuille(5);
-		ori1.setNiveau(Niveau.facile);
-		ori1.setNote(2);
-		ori1.setEtapes(etapes1);
-		ori1.setImageOri("https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Dramatic_Chipmunk.png/220px-Dramatic_Chipmunk.png");
-		ori1.setYoutube("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-		ori1.setCategories(null);
-		ori1.setActif(true);
-
-		origamiRepo.save(ori1);
-
-		Origami ori2 = new Origami();
-
-		ori2.setNom("avion2");
-		ori2.setTempsRea(null);
-		ori2.setNbFeuille(12);
-		ori2.setNiveau(Niveau.moyen);
-		ori2.setNote(8);
-		ori2.setEtapes(etapes2);
-		ori2.setImageOri("https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Doge_homemade_meme.jpg/220px-Doge_homemade_meme.jpg");
-		ori2.setYoutube("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-		ori2.setCategories(null);
-		ori2.setActif(true);
-
-		origamiRepo.save(ori2);
+		// Image Origami creation
 
 		// Admin creation
 
@@ -138,36 +105,83 @@ public class OrigamiApplicationTests {
 
 		adminRepo.save(dolores);
 
-		// Categorie creation
+		// Origami creation
 
+		Origami ori1 = new Origami();
+
+		ori1.setNom("Origami 1");
+		ori1.setTempsRea("30 minutes");
+		ori1.setNbFeuille(5);
+		ori1.setNiveau(Niveau.facile);
+		ori1.setNote(2);
+		ori1.setEtapes(etapes1);
+		ori1.setImageOri(
+				"https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Dramatic_Chipmunk.png/220px-Dramatic_Chipmunk.png");
+		ori1.setYoutube("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+		ori1.setActif(true);
+
+		origamiRepo.save(ori1);
+
+		Origami ori2 = new Origami();
+
+		ori2.setNom("Origami 2");
+		ori2.setTempsRea("1 heure");
+		ori2.setNbFeuille(12);
+		ori2.setNiveau(Niveau.moyen);
+		ori2.setNote(8);
+		ori2.setEtapes(etapes2);
+		ori2.setImageOri(
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Doge_homemade_meme.jpg/220px-Doge_homemade_meme.jpg");
+		ori2.setYoutube("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+		ori2.setActif(true);
+
+		origamiRepo.save(ori2);
+		
+		
+
+		// Categorie creation
+		// SuperCats creation
 		Categorie supercat1 = new Categorie();
 		supercat1.setNom("Animal");
 		supercat1.setSuperCat(null);
-		supercat1.setSuperCats(cat1, cat2);
 		supercat1.setOrigamis(null);
+
+		categorieRepo.save(supercat1);
 		
+		supercat1.setNom("Animaux");
 		categorieRepo.save(supercat1);
 
-		
+		// Liste Origami pour la categorie 1
+		List<Origami> listOrigamiCat1 = new ArrayList<>();
+		listOrigamiCat1.add(ori1);
+
+		origamiRepo.saveAll(listOrigamiCat1);
+
+		// Liste Origami pour la categorie 1
+		List<Origami> listOrigamiCat2 = new ArrayList<>();
+		listOrigamiCat2.add(ori2);
+
+		origamiRepo.saveAll(listOrigamiCat2);
+
 		Categorie cat1 = new Categorie();
 		cat1.setNom("Oiseaux");
 		cat1.setSuperCat(supercat1);
-		cat1.setSuperCats(null);
-		cat1.setOrigamis(null);
+		cat1.setOrigamis(listOrigamiCat1);
 
 		categorieRepo.save(cat1);
 
 		Categorie cat2 = new Categorie();
 		cat2.setNom("Reptiles");
 		cat2.setSuperCat(supercat1);
-		cat2.setSuperCats(null);
-		cat2.setOrigamis(null);
+		cat2.setOrigamis(listOrigamiCat2);
 
 		categorieRepo.save(cat2);
-		
 
+		List<Categorie> superCats1 = new ArrayList<>();
+		superCats1.add(cat1);
+		superCats1.add(cat2);
 
-		
+		categorieRepo.saveAll(superCats1);
 
 	}
 }
