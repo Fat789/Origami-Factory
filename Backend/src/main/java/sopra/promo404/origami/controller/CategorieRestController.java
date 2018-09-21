afc650117b233cc1e6c009ac73f970f62fc095fa
Fat <baptiste.6@hotmail.com>
2018-09-21 12:21:01 +0200

    maj

--- Backend/src/main/java/sopra/promo404/origami/controller/CategorieRestController.java
+++ Backend/src/main/java/sopra/promo404/origami/controller/CategorieRestController.java
@@ -3,6 +3,7 @@ package sopra.promo404.origami.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import sopra.promo404.origami.model.Categorie;
 import sopra.promo404.origami.model.Views;
 import sopra.promo404.origami.repository.IRepoCategorie;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/categorie")
 public class CategorieRestController {
