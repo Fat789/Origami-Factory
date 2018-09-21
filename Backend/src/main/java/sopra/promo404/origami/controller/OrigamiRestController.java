938349dcfd9fb527338439fe708f6e0ae5a747d5
Fat <baptiste.6@hotmail.com>
2018-09-21 12:01:34 +0200

    CrossOrigin sur controller

--- Backend/src/main/java/sopra/promo404/origami/controller/OrigamiRestController.java
+++ Backend/src/main/java/sopra/promo404/origami/controller/OrigamiRestController.java
@@ -3,6 +3,7 @@ package sopra.promo404.origami.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import sopra.promo404.origami.model.Origami;
 import sopra.promo404.origami.model.Views;
 import sopra.promo404.origami.repository.IRepoOrigami;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/origami")
 public class OrigamiRestController {
