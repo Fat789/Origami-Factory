484e526bc0a2a64bb9c619f7c502b35aef4d02d9
Adrien <adrien.lavelle@hotmail.fr>
2018-09-21 12:04:22 +0200

    up

--- Frontend/Origami-app/src/app/categorie/categorie.ts
+++ Frontend/Origami-app/src/app/categorie/categorie.ts
@@ -4,7 +4,7 @@ export class Categorie {
     public nom: string;
 
 
-    constructor(id: number, nom: string) {
+    constructor(id?: number, nom?: string) {
         this.id = id;
         this.nom = nom;
     }
